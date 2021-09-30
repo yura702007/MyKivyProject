b924c275e95b2a8ab06e675349c902503d51d074
yura702007 <82802568+yura702007@users.noreply.github.com>
2021-09-30 19:48:04 +0300

    prob DropDown

--- my_menu/menu.py
+++ my_menu/menu.py
@@ -1,15 +1,31 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.dropdown import DropDown
+from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
+from kivy.base import runTouchApp
+
+
+class UserSettings(DropDown):
+    pass
 
 
 class MyWidget(BoxLayout):
-    pass
+    my_settings = ObjectProperty()
+
+    def show_user_settings(self):
+        dp = UserSettings()
+        btn = Button(text='HELLO')
+        btn.bind(on_release=dp.open)
+        dp.bind(on_select=lambda instance, x: setattr(btn, 'text', x))
+        runTouchApp(btn)
 
 
 class MyMenuApp(App):
 
     def build(self):
         widget = MyWidget()
+        # widget.add_widget(self.settings)
         return widget
 
 
