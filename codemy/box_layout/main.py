c2c8a6c5a08d3c53e0fec52d4f89d971761ce555
yura702007 <82802568+yura702007@users.noreply.github.com>
2021-10-07 18:23:56 +0300

    skeleton file add

--- codemy/box_layout/main.py
+++ codemy/box_layout/main.py
@@ -2,3 +2,20 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang.builder import Builder
+
+Builder.load_file('main.kv')
+
+
+class MyWidget(Widget):
+    pass
+
+
+class MyApp(App):
+    widget = MyWidget()
+
+    def build(self):
+        return self.widget
+
+
+if __name__ == '__main__':
+    MyApp().run()
