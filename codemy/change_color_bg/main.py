1f5e3bf3f1988310e8a7471ad3139dc85f02c5af
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2021-10-09 00:37:31 +0300

    change color in file.py

--- codemy/change_color_bg/main.py
+++ codemy/change_color_bg/main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang.builder import Builder
+from kivy.core.window import Window
 
 Builder.load_file('inherit.kv')
 
@@ -14,6 +15,7 @@ class MyApp(App):
     widget = MyWidget()
 
     def build(self):
+        Window.clearcolor = (0.32, 0.87, 0.61, 1)
         return self.widget
 
 
