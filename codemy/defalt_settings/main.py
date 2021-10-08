95d57fbc621a966142b5244af5ec5e57c9d0f50a
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2021-10-08 22:57:58 +0300

    in main add skeleton

---
+++ codemy/defalt_settings/main.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang.builder import Builder
+
+Builder.load_file('inherit.kv')
+
+
+class MyWidget(Widget):
+    pass
+
+
+class MyApp(App):
+    widget = MyWidget()
+
+    def build(self):
+        return self.widget
+
+
+if __name__ == '__main__':
+    MyApp().run()
