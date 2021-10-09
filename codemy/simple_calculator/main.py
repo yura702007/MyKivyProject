0c901c88cade4e99a422d2c4146dc916922fb6d8
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2021-10-09 07:42:39 +0300

    equals method add

--- codemy/simple_calculator/main.py
+++ codemy/simple_calculator/main.py
@@ -11,8 +11,39 @@ Builder.load_file('calculator.kv')
 
 
 class MyWidget(Widget):
+
     def clear(self):
-        self.ids.calc_input.text = ''
+        self.ids.calc_input.text = '0'
+
+    def press_num_btn(self, symbol):
+        prior = self.ids.calc_input.text
+        if prior == '0':
+            self.ids.calc_input.text = f'{symbol}'
+        else:
+            self.ids.calc_input.text += f'{symbol}'
+
+    def equals(self):
+        expression = self.ids.calc_input.text
+        operands = ['+', '-', '*', '/']
+        operand, exp = '', None
+        result = 0
+        for o in operands:
+            if o in expression:
+                exp = expression.split(o)
+                operand = o
+        if operand == '+':
+            result += int(exp[0]) + int(exp[1])
+        elif operand == '-':
+            result += int(exp[0]) - int(exp[1])
+        elif operand == '*':
+            result += int(exp[0]) * int(exp[1])
+        elif operand == '/':
+            result += int(exp[0]) / int(exp[1])
+        if result == int(result):
+            self.ids.calc_input.text = str(int(result))
+        else:
+            self.ids.calc_input.text = str(result)
+
 
 
 class MyCalculator(App):
