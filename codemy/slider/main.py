42ef4963d2548e7f133219e91ec7978d09bde904
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2021-10-12 02:02:59 +0300

    is ready

--- codemy/slider/main.py
+++ codemy/slider/main.py
@@ -1,12 +1,17 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang.builder import Builder
+# from kivy.properties import ObjectProperty
 
 Builder.load_file('slider.kv')
 
 
 class MyWidget(Widget):
-    pass
+    # slider_text = ObjectProperty()
+
+    def slider_it(self, *args):
+        self.slider_text.font_size = 5 * args[1]
+        self.slider_text.text = str(args[1])
 
 
 class SliderApp(App):
