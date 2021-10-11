634cf4cab6e982b569f0620b74b8cef42b981ee9
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2021-10-11 20:25:27 +0300

    init

---
+++ codemy/round_button/main.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang.builder import Builder
+
+Builder.load_file('round_btn.kv')
+
+
+class MyWidget(Widget):
+    pass
+
+
+class MyApp(App):
+    def build(self):
+        return MyWidget()
+
+
+if __name__ == '__main__':
+    MyApp().run()
