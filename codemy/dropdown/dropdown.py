46fca272892dbc13b02902e9e04d56098ac0e996
yura702007 <82802568+yura702007@users.noreply.github.com>
2021-10-13 17:26:25 +0300

    is ready

--- codemy/dropdown/dropdown.py
+++ codemy/dropdown/dropdown.py
@@ -1,11 +1,13 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang.builder import Builder
+from kivy.properties import ObjectProperty
 
 Builder.load_file('main.kv')
 
 
 class MyWidget(Widget):
+
     def spinner_clicked(self, value):
         self.ids.click_label.text = f'You selected: {value}'
 
