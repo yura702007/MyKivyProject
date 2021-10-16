d96d5f4581821f0d3e38111907fe03ac7df4f51e
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2021-10-16 16:47:22 +0300

    is ready

--- codemy/progress_bar/progress_bar.py
+++ codemy/progress_bar/progress_bar.py
@@ -6,7 +6,15 @@ Builder.load_file('progress_bar.kv')
 
 
 class MyWidget(Widget):
-    pass
+
+    def press_it(self):
+        v = self.ids.my_bar.value
+        if v < 1:
+            v += .25
+        else:
+            v = 0
+        self.ids.my_bar.value = v
+        self.ids.status.text = f'{v * 100}%'
 
 
 class MySomeApp(App):
