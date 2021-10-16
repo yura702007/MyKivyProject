91f635473025a2e9abd1efa85c9d56f4094a444e
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2021-10-16 13:18:56 +0300

    add animate_it method

--- codemy/animation/animation.py
+++ codemy/animation/animation.py
@@ -1,12 +1,14 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang.builder import Builder
+from kivy.animation import Animation
 
 Builder.load_file('animation.kv')
 
 
 class MyWidget(Widget):
-    pass
+    def animate_it(self):
+        pass
 
 
 class MySomeApp(App):
