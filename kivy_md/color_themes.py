cf69417ec7ada2687a8cd046ca01b819b1da07cd
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2021-10-18 06:04:50 +0300

    dark theme

--- kivy_md/color_themes.py
+++ kivy_md/color_themes.py
@@ -4,6 +4,7 @@ from kivymd.app import MDApp
 
 class MainApp(MDApp):
     def build(self):
+        self.theme_cls.theme_style = 'Dark'
         self.theme_cls.primary_palette = 'Blue'
         return Builder.load_file('color_themes.kv')
 
