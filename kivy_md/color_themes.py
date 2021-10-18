1f707d9f09abcf77f07977bcfd434b735981f00e
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2021-10-18 05:58:03 +0300

    init

---
+++ kivy_md/color_themes.py
@@ -0,0 +1,12 @@
+from kivy.lang.builder import Builder
+from kivymd.app import MDApp
+
+
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.primary_palette = 'Blue'
+        return Builder.load_file('color_themes.kv')
+
+
+if __name__ == '__main__':
+    MainApp().run()
