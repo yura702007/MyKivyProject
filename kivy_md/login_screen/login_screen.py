844d4f7601e1f22120b1bd7c84ab251bca487281
yura702007 <82802568+yura702007@users.noreply.github.com>
2021-10-19 11:27:34 +0300

    add logger and clear methods

--- kivy_md/login_screen/login_screen.py
+++ kivy_md/login_screen/login_screen.py
@@ -8,6 +8,15 @@ class MyLoginApp(MDApp):
         self.theme_cls.primary_palette = 'BlueGray'
         return Builder.load_file('login_screen.kv')
 
+    def logger(self):
+        name = self.root.ids.user.text
+        self.root.ids.welcome_label.text = f'Sup {name}!'
+
+    def clear(self):
+        self.root.ids.welcome_label.text = 'Welcome'
+        self.root.ids.user.text = ''
+        self.root.ids.password.text = ''
+
 
 if __name__ == '__main__':
     MyLoginApp().run()
