e51d8fb28ef2233bdbca3ec1d8e879cee615cae9
yura702007 <82802568+yura702007@users.noreply.github.com>
2021-10-19 17:58:13 +0300

    is ready

--- kivy_md/speed_dial_button_menu/dial_menu.py
+++ kivy_md/speed_dial_button_menu/dial_menu.py
@@ -1,8 +1,23 @@
+from pprint import pprint
+
 from kivy.lang.builder import Builder
 from kivymd.app import MDApp
 
 
 class MyDialMenuApp(MDApp):
+    data = {
+        'Python': 'language-python',
+        'Ruby': 'language-ruby',
+        'JS': 'language-javascript'
+    }
+
+    def callback(self, instance):
+        text = instance.icon
+        for key, value in self.data.items():
+            if text == value:
+                self.root.ids.my_label.text = f'You pressed {key}'
+                break
+
     def build(self):
         self.theme_cls.theme_style = 'Dark'
         self.theme_cls.primary_palette = 'Blue'
