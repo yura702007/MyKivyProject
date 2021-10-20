030b0591de76124ba028a22f124aab97e1ca042c
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2021-10-21 02:35:33 +0300

    add show_alert_dialog method

--- kivy_md/alert_dialog_boxes/alert_dialog_boxes.py
+++ kivy_md/alert_dialog_boxes/alert_dialog_boxes.py
@@ -1,12 +1,31 @@
 from kivy.lang.builder import Builder
 from kivymd.app import MDApp
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton
 
 
 class MyDialogBoxesApp(MDApp):
+    dialog = None
+
     def build(self):
         self.theme_cls.theme_style = 'Dark'
         self.theme_cls.primary_palette = 'Blue'
         return Builder.load_file('alert_dialog_boxes.kv')
+
+    def show_alert_dialog(self):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                text='Pretty Neat, Right',
+                buttons=[
+                    MDFlatButton(
+                        text='CANCEL', text_color=self.theme_cls.primary_color
+                    ),
+                    MDRectangleFlatButton(
+                        text="Yes, It'S Neat!", text_color=self.theme_cls.primary_color
+                    )
+                ]
+            )
+        self.dialog.open()
 
 
 if __name__ == '__main__':
