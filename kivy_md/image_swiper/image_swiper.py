a675fb16ae0919e7291145ca991b632e01e30b7c
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2021-10-21 05:42:12 +0300

    is ready

--- kivy_md/image_swiper/image_swiper.py
+++ kivy_md/image_swiper/image_swiper.py
@@ -1,11 +1,29 @@
 from kivy.lang.builder import Builder
 from kivymd.app import MDApp
+import os
 
 
 class MyImageSwiper(MDApp):
+    img_dir = 'images/'
+    img_paths = []
+    count_img = 0
+
     def build(self):
+        self.paths_constructor()
         self.theme_cls.primary_palette = 'Blue'
         return Builder.load_file('image_swiper.kv')
+
+    def paths_constructor(self):
+        paths = next(os.walk('images'))[-1]
+        for path in paths:
+            self.img_paths.append(self.img_dir + path)
+
+    def listing_image(self):
+        if self.count_img < len(self.img_paths) - 1:
+            self.count_img += 1
+        else:
+            self.count_img = 0
+        self.root.ids.my_image.source = self.img_paths[self.count_img]
 
 
 if __name__ == '__main__':
