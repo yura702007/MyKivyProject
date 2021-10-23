cd5f3323d97c2d0b1e68c80ba3f2781dff9288be
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2021-10-23 19:01:13 +0300

    is ready

--- kivy_md/time_picker/time_picker.py
+++ kivy_md/time_picker/time_picker.py
@@ -7,8 +7,18 @@ class MyTimePickerApp(MDApp):
     def build(self):
         return Builder.load_file('time_picker.kv')
 
+    def get_time(self, instance, time):
+        self.root.ids.my_label.text = f'{time}'
+
+    def on_cancel(self, instance, time):
+        self.root.ids.my_label.text = 'You clicked Cancel'
+
     def open_time_picker(self):
+        from datetime import datetime
+        default_time = datetime.strptime('4:20:15', '%H:%M:%S').time()
         time_dialog = MDTimePicker()
+        time_dialog.set_time(default_time)
+        time_dialog.bind(on_cancel=self.on_cancel, time=self.get_time)
         time_dialog.open()
 
 
