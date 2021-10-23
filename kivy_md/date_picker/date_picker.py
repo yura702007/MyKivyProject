7410013b2b6dffb15cc13fe93e07ec893ec2cf12
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2021-10-23 16:51:08 +0300

    is ready

--- kivy_md/date_picker/date_picker.py
+++ kivy_md/date_picker/date_picker.py
@@ -1,10 +1,15 @@
 from kivy.lang.builder import Builder
 from kivymd.app import MDApp
+from kivymd.uix.picker import MDDatePicker
 
 
 class MyDatePickerApp(MDApp):
     def build(self):
         return Builder.load_file('date_picker.kv')
+
+    def open_date_picker(self):
+        date_dialog = MDDatePicker()
+        date_dialog.open()
 
 
 if __name__ == '__main__':
